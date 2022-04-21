8978e6e7ed4e5bd0b993fd8c4c4cb6dac6bc5da4
Janani1403 <janani.haran1403@gmail.com>
2022-04-21 23:41:02 +0530

    .NET MVC Project from AWS Assignment

---
+++ AppDb.cs
@@ -0,0 +1,17 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace WebFramework
+{
+    public class AppDb
+    {
+        public MySqlConnection Connection { get; }
+
+        public AppDb(string connectionString)
+        {
+            Connection = new MySqlConnection(connectionString);
+            Connection.Open();
+        }
+
+        public void Dispose() => Connection.Dispose();
+    }
+}
